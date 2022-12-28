1c46172d829283ecf36b23167d7c77bb8c0a11cb
Breno Carneiro <102473053+BrenoCarneiro@users.noreply.github.com>
2022-12-28 15:17:48 -0300

    Add files via upload

---
+++ Models/Smartphone.cs
@@ -0,0 +1,28 @@
+﻿
+namespace DesafioPOO.Models
+{
+    public abstract class Smartphone
+    {
+        public Smartphone(string numero, string modelo, string imei, int memoria) 
+        { 
+            Numero= numero;
+            Modelo= modelo;
+            IMEI= imei;
+            Memoria= memoria;
+        }
+        public string Numero { get; set; }
+        protected string Modelo;
+        protected string IMEI;
+        protected int Memoria;
+
+        public void Ligar()
+        {
+            Console.WriteLine("Ligando...");
+        }
+        public void ReceberLigacao()
+        {
+            Console.WriteLine("Recebendo ligação...");
+        }
+        public abstract void InstalarAplicativo(string nome);
+    }
+}
